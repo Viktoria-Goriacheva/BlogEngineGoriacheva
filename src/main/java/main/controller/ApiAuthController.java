c3d272f13e20fd933af736310386fee3b77a7fc5
Горячева Виктория <grumbla@yandex.ru>
2022-08-13 15:15:09 +0300

    added captcha

--- src/main/java/main/controller/ApiAuthController.java
+++ src/main/java/main/controller/ApiAuthController.java
@@ -2,8 +2,9 @@ package main.controller;
 
 import lombok.RequiredArgsConstructor;
 import main.api.response.AuthCheckResponse;
+import main.api.response.CaptchaResponse;
 import main.service.AuthCheckService;
-import org.springframework.http.HttpStatus;
+import main.service.CaptchaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiAuthController {
 
   private final AuthCheckService authCheckService;
+  private final CaptchaService captchaService;
 
   @GetMapping("/check")
   public ResponseEntity<AuthCheckResponse> authCheck() {
-    return new ResponseEntity<>(authCheckService.getAuthCheck(), HttpStatus.OK);
+    return ResponseEntity.ok(authCheckService.getAuthCheck());
+  }
+
+  @GetMapping("/captcha")
+  public ResponseEntity<CaptchaResponse> getCaptcha() {
+    return ResponseEntity.ok(captchaService.getCaptchaCode());
   }
 }
 
--- src/main/java/main/repository/CaptchaCodeRepository.java
+++ src/main/java/main/repository/CaptchaCodeRepository.java
@@ -1,8 +1,12 @@
 package main.repository;
 
+import java.util.Optional;
 import main.model.CaptchaCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CaptchaCodeRepository extends JpaRepository<CaptchaCode, Integer> {
 
+  Optional<CaptchaCode> findBySecretCode(String code);
+
+  boolean existsBySecretCode(String code);
 }
